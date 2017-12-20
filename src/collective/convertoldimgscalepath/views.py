cdc3fe38cadafa671ec0a921074ece73a6002c83
Peter Holzer <peter.holzer@agitator.com>
2017-12-20 19:34:18 +0100

    fix regex, never underestimate the “r”

--- src/collective/convertoldimgscalepath/views.py
+++ src/collective/convertoldimgscalepath/views.py
@@ -19,7 +19,7 @@ class ConvertScalePathsView(BrowserView):
             obj = item.getObject()
             if obj.text and '/image_' in obj.text.raw:
                 obj.text = RichTextValue(
-                    re.sub("(\/image_)(.*?)", "/@@images/image/\2",
+                    re.sub(r"(\/image_)(.*?)", r"/@@images/image/\2",
                            obj.text.raw),
                     mimeType=obj.text.mimeType,
                     encoding=obj.text.encoding,
